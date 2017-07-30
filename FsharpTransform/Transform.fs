f6d927d56ae6f12d02746973b4f6bb554bbbf3ea
Jeppe Dickow <Jeppedic@gmail.com>
2017-07-30 11:24:16 +0200

    Added standard headers to the .fs files.

--- FsharpTransform/Transform.fs
+++ FsharpTransform/Transform.fs
@@ -28,16 +28,5 @@
 
 namespace FsharpTransform
 
-module AST = 
- type Property = 
- | Property of string
- type PropertyTransform = 
- | PropertyTransform of (Property * Property)
- type Transformation = 
- | Transformation of PropertyTransform list
- type Document = {configuration:Transformation}
-
- let property (s:string) = s |> Property
- let propertytransform (result:(Property * Property)) = result |> PropertyTransform
- let configuration result = result |> Transformation
- let document propertylist : Document = {configuration=propertylist}
+module Transform = 
+    let a = ""
